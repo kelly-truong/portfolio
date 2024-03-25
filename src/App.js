c0eaddd9b1e7974c141f7ddd726db92798adade4
benjaminluan <benjaminluan1@gmail.com>
2024-03-25 17:39:17 -0400

    added reacat-router-dom a nd useNavigate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import './App.scss';
 import About from './sections/About';
 import BentoBox from './sections/BentoBox';
@@ -6,6 +8,7 @@ import Landing from './sections/Landing';
 import Projects from './sections/Projects';
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <div className="side-nav">
