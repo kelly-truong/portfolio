549873920e06edd704b656763c133bb2235d967a
Kelly <kellytruong6973@gmail.com>
2024-03-26 19:00:01 -0400

    pochacco

--- src/sections/Checklist.jsx
+++ src/sections/Checklist.jsx
@@ -1,12 +1,35 @@
-import react, { useState } from 'react'
+import react from 'react'
+import './checklist.scss'
+import pochacco from '../assets/pochacco.jpg'
 
 
 
 
 const Checklist = () => {
-   
+    // const track = document.getElementById("image--track")
+
+    // window.onmousedown = e => {
+    //     track.dataset.mouseDownAt = e.clientX
+    // }
+
+    // window.onmousemove = e => {
+    //     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    //         maxDelta = window.innerWidth / 2
+
+    //     const percentage = (mouseDelta / maxDelta) * 100
+    // }
+
     return < section id='checklist' >
-        
+        <div className="image--track" data-mouse-down-at="0">
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+            <img src={pochacco} alt="" />
+        </div>
     </section >
 }
 export default Checklist
