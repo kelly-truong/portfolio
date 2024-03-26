f9c9352a2b76f2f5c47f584cfd21b8a9b6c2051b
Kelly <kellytruong6973@gmail.com>
2024-03-25 20:31:30 -0400

    check

--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -8,26 +8,26 @@ const Landing = () => {
 
     return < section id='landing' >
         <div className="landing--wrapper">
-            <h1 className="landing__title">POCHACCO & FROGGY</h1>
+            <h1 className="landing--title">POCHACCO & FROGGY</h1>
             <div className="column--wrapper">
                 <div className="left--side">
-                    <h1 className="landing__title-2">NUBNUB</h1>
+                    <h1 className="landing--title-2">NUBNUB</h1>
                     <div className="cinnamoroll">
                         cinnamoroll
                     </div>
                 </div>
                 <div className="right--side">
-                    <div className="landing__ovals oval-1">
+                    <div className="landing--ovals oval-1">
                         <img className="flower" src={flower} alt="" />
-                        <h1 className="landing__title">CHICKEN NUGGIE</h1>
+                        <h1 className="landing--title">CHICKEN NUGGIE</h1>
                     </div>
-                    <div className="landing__ovals oval-2">
+                    <div className="landing--ovals oval-2">
                         <img className="flower" src={flower} alt="" />
-                        <h1 className="landing__title">CHICKEN NUGGIE</h1>
+                        <h1 className="landing--title">CHICKEN NUGGIE</h1>
                     </div>
-                    <div className="landing__ovals oval-3">
+                    <div className="landing--ovals oval-3">
                         <img className="flower" src={flower} alt="" />
-                        <h1 className="landing__title">CHICKEN NUGGIE</h1>
+                        <h1 className="landing--title">CHICKEN NUGGIE</h1>
                     </div>
                     <div className="about--me">
                         <h2>Kelly Truong</h2>
