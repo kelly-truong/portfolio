7f6237a58e2e2ccc59e58813f45766ee461665f3
Kelly <kellytruong6973@gmail.com>
2024-03-25 23:13:42 -0400

    pink

--- src/sections/Checklist.jsx
+++ src/sections/Checklist.jsx
@@ -1,75 +1,12 @@
 import react, { useState } from 'react'
-import './checklist.scss'
-import heart from '../assets/heart.svg'
 
-const projects = [
-    {
-        name: "Library",
-        desc: 'descript',
-        img: 'imgsrc'
-    },
-    {
-        name: "Netflix Clone",
-        desc: 'descript',
-        img: 'imgsrc'
-    },
-    {
-        name: "Treact",
-        desc: 'descript',
-        img: 'imgsrc'
-    },
-    {
-        name: "Todo List",
-        desc: 'descript',
-        img: 'imgsrc'
-    }
-]
+
+
 
 const Checklist = () => {
-    const [project, setProject] = useState("")
-
-    const updateProject = (projects) => {
-
-    }
-
+   
     return < section id='checklist' >
-        <div className="checklist--wrapper">
-            <div className="checklist--title">
-                <h1 className="landing--title">Projects</h1>
-            </div>
-
-            {/* {projects.map(project => ( */}
-                <div className="columns">
-                    <div className="column-1">
-                        <div className="project__name--box" onClick={updateProject} >
-                            <img className="heart" src={heart} alt="" />
-                            <span className="project--name">Library</span>
-                        </div>
-                        <div className="project__name--box">
-                            <img className="heart" src={heart} alt="" />
-                            <span className="project--name">Netflix Clone</span>
-                        </div>
-                        <div className="project__name--box">
-                            <img className="heart" src={heart} alt="" />
-                            <span className="project--name">Treact</span>
-                        </div>
-                        <div className="project__name--box">
-                            <img className="heart" src={heart} alt="" />
-                            <span className="project--name">Todo List</span>
-                        </div>
-                    </div>
-                    <div className="column-2">
-                        <img className="project--img" src="https://www.smokonow.com/cdn/shop/products/ecCr_pTc.jpg?v=1669926078" alt="" />
-                    </div>
-                    <div className="column-3">
-                        <p className="project--description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ratione deserunt repellendus at. Natus, deserunt? Ut quaerat dolorem adipisci, quod eos exercitationem hic eaque harum numquam impedit dolor illum libero voluptatum odit beatae sunt officia cum ipsam rem aut tempora!
-                        </p>
-                    </div>
-                </div>
-            {/* ))} */}
-        </div>
-
+        
     </section >
 }
 export default Checklist
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,13 +1,64 @@
-import react from 'react'
+import react, { useState } from 'react'
+import './projects.scss'
+import heart from '../assets/heart.svg'
 
-
-
+const projects = [
+    {
+        name: "Library",
+        desc: 'library project',
+        img: 'https://www.smokonow.com/cdn/shop/products/ecCr_pTc.jpg?v=1669926078'
+    },
+    {
+        name: "Netflix Clone",
+        desc: 'netflix project',
+        img: 'imgsrc'
+    },
+    {
+        name: "Treact",
+        desc: 'treact project',
+        img: 'imgsrc'
+    },
+    {
+        name: "Todo List",
+        desc: 'todo project',
+        img: 'imgsrc'
+    }
+]
 
 const Projects = () => {
+    const [selectedProject, setSelectedProject] = useState(projects[0])
+
+    const updateProject = (project) => {
+        setSelectedProject(project)
+        console.log(selectedProject)
+    }
+
 
     return < section id='projects' >
+        <div className="projects--wrapper">
+            <div>
+                <h1 className="projects--title">Projects</h1>
+            </div>
 
-
+            <div className="columns">
+                <div className="column-1">
+                    {projects.map(project => (
+                        <div className={(`project__name--box && ${selectedProject === project ? "selected" : ""}`)} onClick={() => updateProject(project)}>
+                            <img className="heart" src={heart} alt="" />
+                            <span className="project--name">{project.name}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="column-2">
+                    <img className={"project--img"} src={selectedProject.img} alt="" />
+                </div>
+                <div className="column-3">
+                    <p className="project--description">
+                        {selectedProject.desc}
+                    </p>
+                </div>
+            </div>
+        </div>
     </section >
 }
 export default Projects
