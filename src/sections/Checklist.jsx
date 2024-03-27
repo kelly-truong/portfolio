e87db2276b532b9b3b951eabfb5b86aa0c7a8045
benjaminluan <benjaminluan1@gmail.com>
2024-03-26 20:17:52 -0400

    fix bug

--- src/sections/Checklist.jsx
+++ src/sections/Checklist.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import react, { useEffect, useRef } from 'react'
 import './checklist.scss'
 import pochacco from '../assets/pochacco.jpg'
 
@@ -6,42 +6,60 @@ import pochacco from '../assets/pochacco.jpg'
 
 
 const Checklist = () => {
-    const track = document.getElementById("image-track")
+    const trackRef = useRef(null);
 
-    window.onmousedown = e => {
-        track.dataset.mouseDownAt = e.clientX
-    }
 
-    window.onmouseup = () => {
-        track.dataset.mouseDownAt = "0"
-        track.dataset.prevPercentage = track.dataset.percentage
-    }
 
-    window.onmousemove = e => {
-        if (track.dataset.mouseDownAt === "0") return
+    useEffect(() => {
+        if (trackRef) {
+            const updateMouseDownDataSet = (e) => {
+                trackRef.current.dataset.mouseDownAt = e.clientX
+            }
 
-        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-            maxDelta = window.innerWidth / 2
 
-        const percentage = (mouseDelta / maxDelta) * -100
-        const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage
-        nextPercentage = Math.max(-100, Math.min(0, nextPercentage))
+            const updateMouseUpDataSet = (e) => {
+                trackRef.current.dataset.mouseDownAt = "0"
+                trackRef.current.dataset.prevPercentage = trackRef.current.dataset.percentage
+            }
 
-        track.dataset.percentage = nextPercentage
 
-        track.animate({
-            transform: `translate(${nextPercentage}%, -50%)`
-        }, { duration: 1200, fill: "fowards" })
+            const updateMouseMove = (e) => {
+                if (trackRef.current.dataset.mouseDownAt === "0") return
 
-        for (const image of track.getElementsByClassName("image")) {
-            image.animate({
-                objectPosition: `${nextPercentage}% center`
-            }, { duration: 1200, fill: "fowards" })
+                let mouseDelta = parseFloat(trackRef.current.dataset.mouseDownAt) - e.clientX
+                let maxDelta = window.innerWidth / 2
+
+                const percentage = (mouseDelta / maxDelta) * -100
+                let nextPercentage = parseFloat(trackRef.current.dataset.prevPercentage) + percentage
+                nextPercentage = Math.max(-100, Math.min(0, nextPercentage))
+
+                trackRef.current.dataset.percentage = nextPercentage
+
+                trackRef.current.animate({
+                    transform: `translate(${nextPercentage}%, -50%)`
+                }, { duration: 1200, fill: "forwards" })
+
+                for (const image of trackRef.current.getElementsByClassName("image")) {
+                    image.animate({
+                        objectPosition: `${nextPercentage}% center`
+                    }, { duration: 1200, fill: "forwards" })
+                }
+            }
+
+            window.addEventListener("mousedown", updateMouseDownDataSet)
+            window.addEventListener("mouseup", updateMouseUpDataSet)
+            window.addEventListener("mousemove", updateMouseMove)
+
+            return () => {
+                window.removeEventListener("mousedown", updateMouseDownDataSet)
+                window.removeEventListener("mouseup", updateMouseUpDataSet)
+                window.removeEventListener("mousemove", updateMouseMove)
+            }
         }
-    }
+    }, [trackRef])
 
-    return < section id='checklist' >
-        <div className="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+    return <section id='checklist' >
+        <div ref={trackRef} className="image-track clickable" data-mouse-down-at="0" data-prev-percentage="0">
             <img className="image" src={pochacco} alt="" draggable="false" />
             <img className="image" src={pochacco} alt="" draggable="false" />
             <img className="image" src={pochacco} alt="" draggable="false" />
